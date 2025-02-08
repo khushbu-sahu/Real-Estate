c66a966664c17d9da84b478cd6d4dd637d0f3c53
khushbu-sahu <khushboosahu15@outlook.com>
2025-02-08 23:20:58 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,10 +12,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
-const app = express()
+const app = express();
+// we use expesss.json to parse the request body in json format , when we not using this then we can't get the data from the server we get an undefined
+
 
 app.listen(3000 , () =>{
     console.log("server is running on port 3000")
 })
+app.use(express.json())
+app.use('/api/user', userRouter)
 
-app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter )
+
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,6 +24,7 @@ export default function Header() {
                             Home
                         </li>
                     </Link>
+                    
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                     </Link>

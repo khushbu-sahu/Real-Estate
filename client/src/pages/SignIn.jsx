7b28591fa263f186b7aad8886aee1712388c6d58
khushbu-sahu <khushboosahu15@outlook.com>
2025-02-11 12:26:46 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import { Link , useNavigate } from 'react-router-dom'
-
+import { useDispatch } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+import { useSelector } from 'react-redux'
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
     setFormData({
       //here we are using spead operator to store the previous data in formData and add the new data to it and through this we can update the state variable and we use e.target.id to get the id of the input field and e.target.value to get the value of the input field and we use this id and value to update the state variable
@@ -18,7 +22,8 @@ export default function SignIn() {
     e.preventDefault();
 
     try{
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart())
     const res = await fetch('api/auth/signin' , 
       {
         method: 'POST',
@@ -32,17 +37,20 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data)
       if(data.success === false){
-        setLoading(false);
-        setError(data.message);
+        // setLoading(false);
+        // setError(data.message);
+      dispatch(signInFailure(data.message)) ;
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
 
     }catch(error){
-      setLoading(false);
-      setError(error.message)
+      // setLoading(false);
+      // setError(error.message)
+      dispatch(signInFailure(error.message))
 
     }
   }

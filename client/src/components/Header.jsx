21847a6d605095cb02c75f2df91211fa79d4df4a
khushbu-sahu <khushboosahu15@outlook.com>
2025-02-13 15:23:46 +0530

    profile page private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,40 +1,57 @@
-import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-    return (
-        <header className='bg-slate-200 shadow-md '>
-            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-                {/* text-sm for mobile and sm:text-xl for desktop */}
-                <Link to='/'>
-                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Sahand</span>
-                        <span className='text-slate-700'>Estate</span>
-                    </h1>
-                </Link>
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <header className="bg-slate-200 shadow-md ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        {/* text-sm for mobile and sm:text-xl for desktop */}
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
 
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center '>
-                    <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                    <FaSearch className='text-slate-600' />
-                </form>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
 
-                <ul className='flex gap-4'>
-                    <Link to='/'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>
-                            Home
-                        </li>
-                    </Link>
-                    
-                    <Link to='/about'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-                    </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
 
-                    <Link to='/sign-in'>
-                        <li className=' sm:inline text-slate-700 hover:underline'>Sign in</li>
-                    </Link>
-                </ul>
-            </div>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
 
-        </header>
-    )
+          <Link to="profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="profile"
+                className="rounded-full h-10 w-10 object-cover"
+              />
+            ) : (
+              <li className=" sm:inline text-slate-700 hover:underline">
+                Sign in
+              </li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,79 +1,94 @@
-import { useState } from 'react'
-import { Link , useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
-import { useSelector } from 'react-redux'
-import OAuth from '../components/OAuth'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 export default function SignIn() {
-  const [formData, setFormData] = useState({})
-  // const [error, setError] = useState(null)
-  // const [loading, setLoading] = useState(false);
-  const {loading, error} = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const handleChange = (e) =>{
+  const handleChange = (e) => {
     setFormData({
       //here we are using spead operator to store the previous data in formData and add the new data to it and through this we can update the state variable and we use e.target.id to get the id of the input field and e.target.value to get the value of the input field and we use this id and value to update the state variable
       ...formData,
       [e.target.id]: e.target.value,
-    })
-  } ;
+    });
+  };
 
-  const handleSubmit =async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    dispatch(signInStart());
 
-    try{
+    try {
       // setLoading(true);
-      dispatch(signInStart())
-    const res = await fetch('api/auth/signin' , 
-      {
-        method: 'POST',
+
+      const res = await fetch("api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-      }
-      );
-      // changes the stringify data to json data 
+      });
+      // changes the stringify data to json data
       const data = await res.json();
-      console.log(data)
-      if(data.success === false){
+      console.log(data);
+      if (data.success === false) {
         // setLoading(false);
         // setError(data.message);
-      dispatch(signInFailure(data.message)) ;
+        dispatch(signInFailure(data.message));
         return;
       }
       // setLoading(false);
       // setError(null);
       dispatch(signInSuccess(data));
-      navigate('/');
-
-    }catch(error){
+      navigate("/");
+    } catch (error) {
       // setLoading(false);
       // setError(error.message)
-      dispatch(signInFailure(error.message))
-
+      dispatch(signInFailure(error.message));
     }
-  }
-  console.log(formData)
+  };
+  console.log(formData);
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85'>
-          {loading ? 'Loading...' : 'Sign In'}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
-        <OAuth/>
+        <OAuth />
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className='text-blue-700 '>Sign up</span>
+          <span className="text-blue-700 ">Sign up</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-  )
+  );
 }
